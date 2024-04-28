e7dc4df1d156079edadefcb849f578f46c0e3c28
Christophe VG <contact@christophe.vg>
2024-04-28 22:24:38 +0200

    dump env from hello app

--- hello/__init__.py
+++ hello/__init__.py
@@ -4,7 +4,7 @@ import os
 from flask import Flask
 
 # setup logging to stdout
-LOG_LEVEL = os.environ.get("LOG_LEVEL") or "INFO"
+LOG_LEVEL = os.environ.get("LOG_LEVEL") or "DEBUG"
 FORMAT    = "[%(name)s] [%(levelname)s] %(message)s"
 DATEFMT   = "%Y-%m-%d %H:%M:%S %z"
 
@@ -13,6 +13,9 @@ formatter = logging.Formatter(FORMAT, DATEFMT)
 logging.getLogger().handlers[0].setFormatter(formatter)
 
 logger = logging.getLogger(__name__)
+
+for k, v in os.environ.items():
+  logger.debug(f"==> {k} = {v}")
 
 # create app
 app = Flask(__name__)
